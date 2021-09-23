70e259cca322c4c2a85e0223fe7ad110c819f732
SantiCHP <santichadmin@gmail.com>
2021-09-24 00:14:06 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Creating a variable that has my age
+        $age = 24;
+
+        //De este modo se puede imprimir en pantalla texto partido en varias lineas
+        echo nl2br("My name is Santi. \r\n I live in Gran Canaria. \r\n I like programmin.");
+
+        // print "Hello " . "World";
+        // echo "Hello ", "World";
+        // echo "Hello World";
+    ?>
+</body>
+</html>
